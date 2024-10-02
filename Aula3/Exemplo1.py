88482df08174202b74870862f0aa4b7ca87d37c2
Guido-Rodrigues <guidorodrigues6@live.com>
2024-10-02 19:50:24 -0300

    Criado Exemplo1
    Exemplo de código para calcular um imposto qualquer com alíquota pré-definida e exibir ao usuário o valor devido.

---
+++ Aula3/Exemplo1.py
@@ -0,0 +1,4 @@
+print("Vamos calcular o imposto! Coloque o valor do produto:")
+valorproduto = float(input())
+valorimposto = valorproduto * 0.1
+print(f"O valor que você terá de pagar em impostos sobre este produto é: R${valorimposto}")
