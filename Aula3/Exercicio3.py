8f16dd073c2035cc5773033678a4aa57ef9d6d14
Guido-Rodrigues <guidorodrigues6@live.com>
2024-10-02 21:43:22 -0300

    Cria o Exercício3
    O exercício consiste em calcular o tamanho do sapato e focinheira de um cachorro em função do tamanho conhecido de sua coleira.

---
+++ Aula3/Exercicio3.py
@@ -0,0 +1,10 @@
+#Faça um programa que calcule o tamanho do sapato de um cachorro sabendo que sermpre será o dobro do tamanho da coleira dele. 
+#Calcule também o tamanho da pata do cachorro
+#Calcule também o tamanho da fucinheira sabendo que é o triplo do tamanho do sapato.
+#O programa deverá pedir o tamanho da coleira do cachorro do usuario e retornar o tamanho do sapato e da fucinheira que o dono deverá comprar.
+
+print("Olá, insira o tamanho da coleira de seu cachorro")
+valorColeira = float(input())
+valorSapato = 2*valorColeira
+valorFucinheira = 3*valorSapato
+print(f"Seu cachorro veste Sapato tamanho {valorSapato} e Fucinheira tamanho {valorFucinheira} ")
