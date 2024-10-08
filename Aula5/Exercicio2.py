b2249e69ade4262dbefa6f2d3192e8d500b9f62c
Guido-Rodrigues <guidorodrigues6@live.com>
2024-10-07 21:58:52 -0300

    Atualização exercicio2

--- Aula5/Exercicio2.py
+++ Aula5/Exercicio2.py
@@ -13,9 +13,9 @@ print("Olá escolha um serviço:\n1- Lavagem completa: R$ 50.00\n2- Lavagem bás
 entrada = int(input())
 #Retorna erro caso o valor não seja 1 ou 2
 if(entrada <1 or entrada >2):
-    print("Entrada inválida!")
-#Caso o valor seja 1 registra o preço na variavel do valor do pedido e oferece o "pretinho"
+    print("Entrada inválida!!! Tente novamente.")
 else:
+    #Caso o valor seja 1 registra o preço na variavel do valor do pedido e oferece o "pretinho"
     if(entrada ==1):
         Vpedido = Vpedido + 50
         print("Gostaria de adicionar o pretinho por mais R$ 5.00? (S ou N)")
@@ -26,10 +26,10 @@ else:
             print(f"Você escolheu Lavagem completa com pretinho. Total: R$ {Vpedido}")
         elif(Selecpretinho == "N"):
             print(f"Você escolheu Lavagem completa sem pretinho Total: R$ {Vpedido}")
-            #Retorna erro caso o valor do input não seja S ou N
+        #Retorna erro caso o valor do input não seja S ou N
         else:
-            print("Entrada inválida!!!")
-            #Caso o valor não seja 1, o pedido só pode ser Lavagem básica, é adicionado então o valor à variável e oferece o "pretinho"
+            print("Entrada inválida!!! Tente novamente.")
+    #Caso o valor não seja 1, o pedido só pode ser Lavagem básica, é adicionado então o valor à variável e oferece o "pretinho"
     else:
         Vpedido = Vpedido + 35
         print("Gostaria de adicionar o pretinho por mais R$ 5.00? (S ou N)")
@@ -40,6 +40,6 @@ else:
             print(f"Você escolheu Lavagem básica com pretinho. Total: R$ {Vpedido}")
         elif(Selecpretinho =="N"):
             print(f"Você selecionou lavagem básica sem pretinho. Total: R$ {Vpedido}")
-            #Retorna erro caso o valor do input não seja S ou N
+        #Retorna erro caso o valor do input não seja S ou N
         else:
-            print("Entrada inválida!!!")
+            print("Entrada inválida!!! Tente novamente.")
