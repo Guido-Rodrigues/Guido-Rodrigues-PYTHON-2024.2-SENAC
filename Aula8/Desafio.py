3ae326dfbf1c5f98aa13bf3438aaba0d6ff04931
Guido-Rodrigues <guidorodrigues6@live.com>
2024-10-15 19:37:22 -0300

    Desafio aula 8
    Completa desafio aula 8: algoritmo que gera a sequencia de Fibonacci

---
+++ Aula8/Desafio.py
@@ -0,0 +1,12 @@
+#faça um programa que imprima os 10 primeiros números da sequência de fibonacci
+#Formula > Fn = Fn-1 + Fn-2
+n = int(input("Por favor insira quantos numeros da sequencia de fibonacci quer: "))
+n1 = 1
+n2 = 2
+print(n1)
+print(n2)
+for i in range(3, n+1):
+    n3 = n1 + n2
+    print (n3)
+    n1 = n2
+    n2 = n3
