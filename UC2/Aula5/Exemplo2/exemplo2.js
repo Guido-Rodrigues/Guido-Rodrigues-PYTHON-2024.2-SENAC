f16cf3507f955699482cc9e58b931bdff1843e2d
Guido-Rodrigues <guidorodrigues6@live.com>
2024-11-04 21:56:35 -0300

    Aula5
    Aula abordou JavaScript

---
+++ UC2/Aula5/Exemplo2/exemplo2.js
@@ -0,0 +1,30 @@
+document.write("Alo Terra!")
+alert("olá")
+console.log("olá tudo bem?")
+
+/*variaveis
+var my_var;
+my_var = "Olá, pessoal";
+alert(my_var);
+my_var = 5;
+alert(my_var);
+my_var = [1, 10, "olá", true];
+alert(my_var);
+*/
+
+var variavel1 = "ola mundo"
+alert(variavel1)
+var variavel2 = "123"
+alert(variavel2)
+console.log(variavel2)
+let variavel3 = "Eu tambem sou uma variavel"
+alert(variavel3)
+
+/*operadores lógicos
+//and: &&
+//or: ||
+*/
+
+document.write("Alo Marte!")
+document.write("<h1>Eu sou um título</h1>")
+document.write('<title>Titulo da pagina</title>')
